012a5bd59daf2825ec1f017c317a34dcc0f07faf
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-08-04 16:54:18 -0400

    Implementação de compente Card para ser usado na view Login

---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Card extends React.Component{
+  render(){
+    return(
+      <div className="card mb-3">
+        <div className="card-header">{this.props.title}</div>
+        <div className="card-header">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Card;
---
+++ src/views/login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from './../components/card';
+
+class Login extends React.Component{
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6" style={{position: 'relative', left: '300px'}}>
+            <div className="bs-docs-section">
+              <Card title="Login">
+                
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login;
