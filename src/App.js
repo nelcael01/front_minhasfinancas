42fd32363a15b345af545a368df37dbb8d2f5838
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-08-05 16:30:46 -0400

    Implementação da view cadastroUsuario

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import 'bootswatch/dist/flatly/bootstrap.css'
 import Login from "./views/login";
 import "./custom.css"
+import CadastroUsuario from "./views/cadastroUsuario";
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        <Login />
+        <CadastroUsuario />
       </div>
       
     )
