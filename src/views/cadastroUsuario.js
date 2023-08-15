1a538328f7e236c22b07dcecf9147e942d38adfa
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-08-14 21:23:50 -0400

    Refatorando validação de forms cadastrar usuarios

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -19,48 +19,22 @@ class CadastroUsuario extends React.Component{
     this.service = new UsuarioService()
   }
 
-  validar(){
-    const msgs = []
-
-    if (!this.state.nome) {
-      msgs.push("O campo Nome é obrigatório")
-    }
-
-    if (!this.state.email) {
-      msgs.push("O campo E-mail é obrigatório")
-    }else if (!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)) {
-      msgs.push("Informe um E-mail válido")
-    }
-
-    if (!this.state.senha) {
-      msgs.push("O campo Senha é obrigatório")
-    }
-
-    if (!this.state.senhaRepetida) {
-      msgs.push("O campo Repita a Senha é obrigatório")
-    }
-
-    if (this.state.senha !== this.state.senhaRepetida) {
-      msgs.push("As senhas digitadas não são iguais")
-    }
-    return msgs
-  }
-
   cadastrar = () =>{
-    const msgs = this.validar()
-
-    if (msgs && msgs.length > 0) {
-      msgs.forEach((msg, i) =>{
-        mensagemErro(msg)
-      })
-      return false
-    }
-
     const usuario = {
       nome: this.state.nome,
       email: this.state.email,
-      senha: this.state.senha
+      senha: this.state.senha,
+      senhaRepetida: this.state.senhaRepetida
     }
+
+    try{
+      this.service.validar(usuario);
+    }catch(erro){
+        const msgs = erro.mensagens;
+        msgs.forEach(msg => mensagemErro(msg));
+        return false;
+    }
+
     this.service.salvar(usuario)
       .then(response =>{
         mensagemSucesso('Usuário cadastrado com sucesso. Faça o login para acessar o sistema')
